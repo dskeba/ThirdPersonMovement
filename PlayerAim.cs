2e724128736038716c351df7ba8c70b0ad5ce7ed
Derek Skeba <frillyoz@gmail.com>
2020-06-15 19:02:13 -0400

    Initial commit

---
+++ PlayerAim.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    public float turnSpeed = 1f;
+    Camera mainCamera;
+    public float targetAngle;
+    public float slerpSpeed;
+    public float horizontal;
+    public float vertical;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
+
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(0f, yawCamera + targetAngle, 0f);
+
+
+        slerpSpeed = turnSpeed * Time.fixedDeltaTime;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera + targetAngle, 0), slerpSpeed);
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera + (horizontal * 90f) + (vertical * 180f), 0), turnSpeed * Time.fixedDeltaTime);
+
+
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        var camera = Camera.main;
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        transform.position += (forward * input.y + right * input.x) * Time.deltaTime * speed;
+
+
+        //this is the direction in the world space we want to move:
+        //var desiredMoveDirection = forward * vertical + right * horizontal;
+
+        //now we can apply the movement:
+        //transform.Translate(desiredMoveDirection * speed * Time.deltaTime);
+
+        float yawCamera = camera.transform.rotation.eulerAngles.y;
+
+        //transform.rotation = Quaternion.Euler(0f, horizontal * 90f, 0f);
+
+/*        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * yawCamera;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            //controller.Move(direction * speed * Time.deltaTime);
+        }*/
+    }
+}
