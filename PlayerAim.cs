f276deb2bd4acd1c3e796c18942f63de02792fff
Derek Skeba <frillyoz@gmail.com>
2020-06-16 01:07:55 -0400

    Update PlayerAim and PlayerMovement to rotate body manually based on aim

--- PlayerAim.cs
+++ PlayerAim.cs
@@ -7,41 +7,52 @@ public class PlayerAim : MonoBehaviour
     public float turnSpeed = 1f;
     Camera mainCamera;
     public float targetAngle;
-    public float slerpSpeed;
     public float horizontal;
     public float vertical;
+
+    private GameObject playerHips;
+    private GameObject playerAbdomen;
+    private GameObject playerTorso;
+
+    private Animator animator;
 
     private void Start()
     {
         mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        animator = GetComponent<Animator>();
+        playerHips = GameObject.FindGameObjectWithTag("PlayerHips");
+        playerAbdomen = GameObject.FindGameObjectWithTag("PlayerAbdomen");
+        playerTorso = GameObject.FindGameObjectWithTag("PlayerTorso");
     }
 
     private void FixedUpdate()
     {
-        //horizontal = Input.GetAxis("Horizontal");
-        //vertical = Input.GetAxis("Vertical");
-
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        animator.SetFloat("VelX", horizontal);
+        animator.SetFloat("VelY", vertical);
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
-
-
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(0f, yawCamera + targetAngle, 0f);
-
-
-        slerpSpeed = turnSpeed * Time.fixedDeltaTime;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera + targetAngle, 0), slerpSpeed);
-
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera + (horizontal * 90f) + (vertical * 180f), 0), turnSpeed * Time.fixedDeltaTime);
-
-
+        if (targetAngle == 180)
+        {
+            targetAngle = 0;
+        }
+        else if (targetAngle > 90)
+        {
+            var offset = targetAngle - 90;
+            targetAngle = -offset;
+        } 
+        else if (targetAngle < -90)
+        {
+            var offset = targetAngle + 90;
+            targetAngle = -offset;
+        }
+        transform.eulerAngles = new Vector3(0, yawCamera + targetAngle, 0);
+        playerHips.transform.localEulerAngles = new Vector3(0f, -targetAngle / 2, 0f);
+        playerAbdomen.transform.localEulerAngles = new Vector3(0f, -targetAngle / 3, 0f);
+        playerTorso.transform.localEulerAngles = new Vector3(0f, -targetAngle / 2, 0f);
     }
 }
